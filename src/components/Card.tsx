074ffbf912fb91245c15140c9267e281c52546bb
MorganGray28 <robertmgray2828@gmail.com>
2023-05-31 19:01:59 -0700

    implement first step of multiform

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
+import { PersonalInfo } from '../types';
 import '../styles/Card.css';
 import Sidebar from './Sidebar';
 import FirstFormStep from './FirstFormStep';
 
 function Card() {
-	const [activeStep, setActiveStep] = useState(1);
+	const [activeStep, setActiveStep] = useState(0);
+	const [personalInfo, setPersonalInfo] = useState({
+		name: '',
+		email: '',
+		phone: '',
+	});
+
+	function goToNextStep() {
+		setActiveStep((prevStep) => prevStep + 1);
+	}
+
+	function updatePersonalInfo(info: PersonalInfo) {
+		setPersonalInfo(info);
+	}
 
 	return (
 		<div className='Card'>
 			<Sidebar activeStep={activeStep} />
 			<div className='card-content-container'>
-				<FirstFormStep />
+				<FirstFormStep goToNextStep={goToNextStep} updatePersonalInfo={updatePersonalInfo} />
 			</div>
 		</div>
 	);
--- src/components/FirstFormStep.tsx
+++ src/components/FirstFormStep.tsx
@@ -1,8 +1,129 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { PersonalInfo } from '../types';
 import '../styles/FormStep.css';
 
-function FirstFormStep() {
-	return <div className='FormStep'></div>;
+type PropsType = {
+	goToNextStep: () => void;
+	updatePersonalInfo: (info: PersonalInfo) => void;
+};
+
+function FirstFormStep({ goToNextStep, updatePersonalInfo }: PropsType) {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [phone, setPhone] = useState('');
+
+	// error states
+	const [nameError, setNameError] = useState<string | null>(null);
+	const [emailError, setEmailError] = useState<string | null>(null);
+	const [phoneError, setPhoneError] = useState<string | null>(null);
+
+	function handleSubmit() {
+		let emptyFieldError = 'This field is required';
+		let isValid = true;
+		if (!name) {
+			setNameError(emptyFieldError);
+			isValid = false;
+		}
+		if (!email) {
+			setEmailError(emptyFieldError);
+			isValid = false;
+		}
+		if (!phone) {
+			setPhoneError(emptyFieldError);
+			isValid = false;
+		}
+
+		if (!validateEmail()) {
+			setEmailError('Invalid email address');
+			isValid = false;
+		}
+
+		if (isValid) {
+			goToNextStep();
+			updatePersonalInfo({ name, email, phone });
+		}
+	}
+
+	// Extremely basic version of email validation
+	function validateEmail() {
+		let isValid = true;
+		let validDomains = ['com', 'org', 'io', 'dev', 'net', 'gov'];
+		let emailFormatted = email.toLowerCase().split('.');
+		if (
+			!validDomains.includes(emailFormatted[emailFormatted.length - 1]) ||
+			!email.includes('@') ||
+			email.includes(' ')
+		) {
+			isValid = false;
+		}
+		return isValid;
+	}
+
+	return (
+		<div className='FormStep'>
+			<h2 className='step-header'>Personal info</h2>
+			<p className='step-subheader'>Please provide your name, email address, and phone number.</p>
+			<form>
+				<div className='form-group'>
+					<div className='label-group'>
+						<label htmlFor=''>Name</label>
+						<p className='error-msg'>{nameError && nameError}</p>
+					</div>
+					<input
+						className='input-text'
+						type='text'
+						id='name'
+						name='name'
+						value={name}
+						onChange={(e) => {
+							setName(e.target.value);
+							setNameError(null);
+						}}
+						placeholder='e.g. Stephen King'
+					/>
+				</div>
+				<div className='form-group'>
+					<div className='label-group'>
+						<label htmlFor=''>Email Address</label>
+						<p className='error-msg'>{emailError && emailError}</p>
+					</div>
+					<input
+						className='input-text'
+						type='text'
+						id='email'
+						name='email'
+						value={email}
+						onChange={(e) => {
+							setEmail(e.target.value);
+							setEmailError(null);
+						}}
+						placeholder='e.g. stephenking@lorem.com'
+					/>
+				</div>
+				<div className='form-group'>
+					<div className='label-group'>
+						<label htmlFor=''>Phone Number</label>
+						<p className='error-msg'>{phoneError && phoneError}</p>
+					</div>
+					<input
+						className='input-text'
+						type='text'
+						id='phone'
+						name='phone'
+						value={phone}
+						onChange={(e) => {
+							setPhone(e.target.value);
+							setPhoneError(null);
+						}}
+						placeholder='e.g. +1 234 567 890'
+					/>
+				</div>
+			</form>
+			<div className='btn-container'>
+				<button onClick={handleSubmit}>Next Step</button>
+			</div>
+		</div>
+	);
 }
 
 export default FirstFormStep;
