2212fce6197e62a219a8ac5b3756621b2840a950
MorganGray28 <robertmgray2828@gmail.com>
2023-06-04 17:21:00 -0700

    add responsive design for sidebar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,9 @@
 import '../styles/App.css';
 import Card from './Card';
-import SidebarMobile from './SidebarMobile';
 
 function App() {
 	return (
 		<div className='App'>
-			<SidebarMobile />
 			<Card />
 		</div>
 	);
